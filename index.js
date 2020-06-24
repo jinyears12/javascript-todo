9dda43c726f5d03a0885b7f964e136a1544d5411
jinyears12 <67301207+jinyears12@users.noreply.github.com>
2020-06-24 19:13:22 +0900

    0624.Wed(clock)

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-const title = document.getElementById("title");
-const clicked = "clicked";
+const clockd = document.querySelector(".js-clockd");
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-title.innerText = "Hello from ha...";
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "June",
+  "July",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
 
-function handleClick() {
-  const hassClass = title.classList.contains(clicked);
-  if (!hassClass) {
-    title.classList.add(clicked);
-  } else {
-    title.classList.remove(clicked);
-  }
+function paintTime(result) {
+  clockd.innerText = result;
 }
 
-title.addEventListener("click", handleClick);
-// console.log(title.innerText);
+function getTime() {
+  const date = new Date();
+  const day = days[date.getDay()];
+  const month = months[date.getMonth()];
+  const today = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const result = `${day} ${month} ${today} ${
+    hours < 10 ? `0${hours}` : hours
+  } : ${minutes < 10 ? `0${minutes}` : minutes} : ${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
+  paintTime(result);
+}
+
+function init() {
+  getTime();
+  setInterval(getTime, 1000);
+}
+
+init();
